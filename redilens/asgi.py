1954d41528bfc010ef61e3c2fb985dd79813528d
ZED <67244328+nickravesh@users.noreply.github.com>
2025-09-01 00:34:20 +0330

    [init]: create initial structure and files

---
+++ redilens/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for redilens project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'redilens.settings')
+
+application = get_asgi_application()
---
+++ redis_monitor/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RedisMonitorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'redis_monitor'
